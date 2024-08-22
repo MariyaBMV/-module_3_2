6408481dba89124d7074d3da46fff7e821e311c5
MariyaBMV <boiko100@mail.ru>
2024-08-22 22:36:02 +1000

    Rabota nad oshibkami
    Sposobi vizova funkcii

--- Sposobi vizova funccii.py
+++ Sposobi vizova funccii.py
@@ -3,7 +3,7 @@
 Попробуем реализовать функцию с подробной логикой.
 
 Создайте функцию send_email, которая принимает 2 обычных аргумента: сообщение и получатель и 1 обязательно именованный аргумент со значением по умолчанию - отправитель.
-Внутри функции реализовать следующую логику:
+Внутри функции реализовать следующую логи"ку:
 Проверка на корректность e-mail отправителя и получателя.
 Проверка на отправку самому себе.
 Проверка на отправителя по умолчанию.
@@ -17,18 +17,15 @@
 За один вызов функции выводится только одно и перечисленных уведомлений! Проверки перечислены по мере выполнения."""
 
 def send_email(message, recipient, sender = "university.help@gmail.com"): # Создала функцию с двумя обычными аргуменатми и одни обязательным со значением по умолчанию.
-    if "@" not in recipient or not recipient.endswith((".com", ".ru", ".net")):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-        return
-    if sender == recipient:
-        print("Нельзя отправить письмо самому себе!")
-        return
-    if sender == "university.help@gmail.com":
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+    if not recipient.endswith(("@",".com",".ru",".net")) or "@" not in recipient:
+        print("Невозможно отправить письмо с адреса <", sender, "> на адрес <", recipient, ">")
+    elif sender == recipient:
+        print ("Нельзя отправить письмо самому себе")
+
     else:
         print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
-
+send_email("КАк дел","testexample.com" )
 send_email("Привет!", "test@example.com")
-send_email("Важное сообщение", "test@example.com", sender="support@company.com")
-send_email("Ошибка", "university.help@gmail.com", sender="university.help@gmail.com")
-send_email("Запрос", "wrong.address", sender="unknown@wrong.domain")
+send_email("Важное сообщение", "test@example.com","support@company.com")
+send_email("Ошибка", "university.help@gmail.com","university.help@gmail.com")
+send_email("Запрос", "wrong.address", "unknown@wrong.domain")
